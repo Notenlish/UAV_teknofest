cafd7075982ca5a3ede48b42a42281476020abc1
Notenlish <71970100+Notenlish@users.noreply.github.com>
2024-06-07 09:47:22 +0300

    bruv

--- m.py
+++ m.py
@@ -3,10 +3,8 @@ import time
 import pyautogui
 
 
-def move_mouse_periodically(interval):
-    c = 0
+def dont_be_afk(interval):
     while True:
-        c += 1
         # Move the mouse slightly to prevent sleep
         pyautogui.write("Hello world! ", interval=0.25)
         time.sleep(interval)
@@ -16,4 +14,4 @@ if __name__ == "__main__":
     # Set the interval in seconds
     interval = 20  # Adjust this as needed
     time.sleep(5)
-    move_mouse_periodically(interval)
+    dont_be_afk(interval)
