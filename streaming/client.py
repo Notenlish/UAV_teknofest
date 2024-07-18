9fb16690d8f9f55142c5994c835423f082da7cb0
Notenlish <71970100+Notenlish@users.noreply.github.com>
2024-07-18 17:02:03 +0300

    just try until it works

--- streaming/client.py
+++ streaming/client.py
@@ -1,26 +1,50 @@
 import cv2
 
-def receive_ffmpeg_stream(port, use_udp=True):
-    protocol = 'udp' if use_udp else 'tcp'
-    url = f'{protocol}://192.168.1.97:{port}'
-    cap = cv2.VideoCapture(url)
 
-    if not cap.isOpened():
-        print("Error: Could not open video stream.")
-        return
+class VideoError(Exception):
+    pass
 
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
 
-        cv2.imshow('Video Stream', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+class VideoReceive:
+    def __init__(self, port, host, use_udp=True) -> None:
+        self.protocol = 'udp' if use_udp else 'tcp'
+        self.url = f'{self.protocol}://{host}:{port}'
 
-    cap.release()
-    cv2.destroyAllWindows()
+    def _start(self, url:str):
+        print("attempting to connect...")
+        cap = cv2.VideoCapture(url)
+        print("started capture")
+        if not cap.isOpened():
+            raise VideoError("My hovercraft is full of eels")
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            cv2.imshow('Video Stream', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+    def start(self):
+        print("starting capture")
+        err_count = 0
+        while err_count < 10:
+            try:
+                self._start(self.url)
+            except VideoError:
+                err_count += 1
+            except:
+                err_count += 10
+                raise Exception("aaaaaaaaaaaaaaaaaaaaa")
 
 if __name__ == "__main__":
     port = 12345  # Should match the port used on the server
-    receive_ffmpeg_stream(port, use_udp=True)
+    use_udp = True
+    host = "129.168.1.100"
+    
+    receive = VideoReceive(port, host, use_udp=use_udp)
+    receive.start()
