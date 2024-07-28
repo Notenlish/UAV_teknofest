bab72b372bf7041c13c26b3cc1d3bc2e5874236a
Notenlish <71970100+Notenlish@users.noreply.github.com>
2024-07-28 19:20:35 +0300

    Create kamera.py

---
+++ kamera.py
@@ -0,0 +1,42 @@
+import subprocess
+import threading
+import random
+
+gst_args = (
+    "gst-launch-1.0",
+    "nvarguscamerasrc", "!",
+    "video/x-raw(memory:NVMM), width=1280, height=720, framerate=30/1, format=NV12", "!",
+    "nvvidconv", "!", "video/x-raw, format=I420", "!",
+    "videoconvert", "!", "video/x-raw, format=RGB", "!",
+    "jpegenc", "!", "filesink", "location=/dev/stdout"
+)
+
+v = random.randint(0, 1000)
+filename = f"output-{v}.mp4"
+
+ffmpeg_args = (
+    "ffmpeg", "-f", "image2pipe", "-vcodec", "mjpeg",
+    "-i", "-", "-vcodec", "libx264", "-pix_fmt", "yuv420p", "-f", "mp4", filename
+)
+
+def run_gstreamer():
+    proc = subprocess.Popen(gst_args, stdout=subprocess.PIPE)
+    return proc
+
+def run_ffmpeg(pipe):
+    proc2 = subprocess.Popen(ffmpeg_args, stdin=pipe)
+    proc2.wait()
+
+print(filename)
+
+gst_proc = run_gstreamer()
+ffmpeg_thread = threading.Thread(target=run_ffmpeg, args=(gst_proc.stdout,))
+
+try:
+    ffmpeg_thread.start()
+    gst_proc.wait()
+    ffmpeg_thread.join()
+except KeyboardInterrupt:
+    gst_proc.terminate()
+    ffmpeg_thread.join()
+
