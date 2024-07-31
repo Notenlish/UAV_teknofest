becaa25f66a501a5c5ace164d34e77d526987536
Notenlish <71970100+Notenlish@users.noreply.github.com>
2024-07-31 15:57:56 +0300

    ğ

--- kamera2.py
+++ kamera2.py
@@ -3,32 +3,59 @@ import threading
 import random
 
 quality = 30  # 0 - 100(highest quality)
-framerate = 30
+
 v = random.randint(0, 1000)
 filename = f"output-{v}.mp4"
 
-
 gst_args = (
     "gst-launch-1.0",
-    "nvarguscamerasrc", "!",
-    "video/x-raw(memory:NVMM), width=1280, height=720, framerate=20/1, format=NV12", "!",
-    "nvjpegenc", f"quality={quality}", "!",
-    "image/jpeg, framerate=20/1", "!",
-    "filesink", "location=/dev/stdout"
+    "nvarguscamerasrc",
+    "!",
+    "video/x-raw(memory:NVMM), width=1280, height=720, framerate=20/1, format=NV12",
+    "!",
+    "nvvidconv",
+    "!",
+    "video/x-raw, format=I420",
+    "!",
+    "videoconvert",
+    "!",
+    "filesink",
+    "location=/dev/stdout",
+    "sync=false",
 )
 
+
 ffmpeg_args = (
-    "ffmpeg", "-f", "image2pipe", "-framerate", "20", "-vcodec", "mjpeg",
-    "-i", "-", "-r", "20", "-vcodec", "libx264", "-preset", "fast", "-pix_fmt", "yuv420p", "-f", "mp4", filename
+    "ffmpeg",
+    "-f",
+    "rawvideo",
+    "-pix_fmt",
+    "yuv420p",
+    "-s",
+    "1280x720",
+    "-r",
+    "20",
+    "-i",
+    "-",
+    "-c:v",
+    "libx264",
+    "-preset",
+    "veryfast",
+    "-crf",
+    f"{quality}",
+    filename,
 )
+
 
 def run_gstreamer():
     proc = subprocess.Popen(gst_args, stdout=subprocess.PIPE, bufsize=10**8)
     return proc
 
+
 def run_ffmpeg(pipe):
     proc2 = subprocess.Popen(ffmpeg_args, stdin=pipe)
     proc2.wait()
+
 
 print(filename)
 
